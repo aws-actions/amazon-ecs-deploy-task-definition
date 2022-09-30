3fbbdf13fc76d4ae12574756d68b567e3885f6f2
humphrey.ndirangu <humphrey.ndirangu@wundermobility.com>
2022-09-30 17:52:41 +0200

    Allow deployments if type=ECS

--- index.js
+++ index.js
@@ -308,7 +308,7 @@ async function run() {
         throw new Error(`Service is ${serviceResponse.status}`);
       }
 
-      if (!serviceResponse.deploymentController) {
+      if (!serviceResponse.deploymentController || serviceResponse.deploymentController === 'ECS') {
         // Service uses the 'ECS' deployment controller, so we can call UpdateService
         await updateEcsService(ecs, clusterName, service, taskDefArn, waitForService, waitForMinutes, forceNewDeployment);
       } else if (serviceResponse.deploymentController.type == 'CODE_DEPLOY') {
